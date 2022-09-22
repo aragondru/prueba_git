391494552734b78f3e391be80e673186cee3b73b
Christian Avalos <christianavalos311997@gmail.com>
2022-09-21 19:39:24 -0600

    primer commit

---
+++ src/main/java/sv/edu/ues/pruebagit/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sv.edu.ues.pruebagit;
+
+/**
+ *
+ * @author avalos
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo");
+    }
+
+}
