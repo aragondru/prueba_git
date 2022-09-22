648c90704add3071cab55da37171e8f3a70f51d7
Alejandro Aragon <aragondru155@gmail.com>
2022-09-21 19:58:28 -0600

    Agregando texto en el println

--- src/main/java/sv/edu/ues/pruebagit/Main.java
+++ src/main/java/sv/edu/ues/pruebagit/Main.java
@@ -15,7 +15,7 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo haciendo un cambio en la ramab");
     }
 
 }
